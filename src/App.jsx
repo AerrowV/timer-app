b530d7761379e4d25b0c768b5004c431ef5213c6
AerrowV <businessasimk@gmail.com>
2025-05-15 10:34:30 +0200

    deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Timer from './components/Timer'
+import Timer from "./components/Timer";
 
 function App() {
   return (
     <div>
       <Timer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
